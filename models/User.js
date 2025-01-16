bdef7891925d8a47a5ac26912e33a7aff72af61f
hussain <hussainalsaffar20@gmail.com>
2025-01-16 13:01:29 +0300

    done

--- models/User.js
+++ models/User.js
@@ -2,10 +2,18 @@ const { model, Schema } = require("mongoose");
 
 const PasswordManager = require("../helper/PasswordManager");
 
-const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["Employee", "Manager", "HR"], required: true },
+    location: String,
+    title: String,
+  },
+  { timestamps: true }
+);
+
 UserSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
     const hashed = await PasswordManager.toHash(this.get("password"));
