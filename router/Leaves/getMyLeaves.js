82620560569f583a30e6f4957729c84301a5e52f
hussain <hussainalsaffar20@gmail.com>
2025-01-20 10:41:19 +0300

    add get leaves

--- router/Leaves/getMyLeaves.js
+++ router/Leaves/getMyLeaves.js
@@ -2,15 +2,19 @@ const express = require("express");
 const Leave = require("../../models/Leaves.js");
 const User = require("../../models/User.js");
 const router = express.Router();
-const { body } = require("express-validator");
+const { requireAuth } = require("../../middleware/index.js");
 
-router.get("/all", async (req, res) => {
+router.get("/", requireAuth, async (req, res) => {
   try {
-    const leaves = await Leave.find();
+    const leaves = await Leave.find({ user_id: req.user.id }).populate(
+      "user_id",
+      "name email"
+    );
     res.status(200).json(leaves);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error", error });
   }
 });
-module.exports = { getLeaveRouter: router };
+
+module.exports = { getMyLeaveRouter: router };
--- router/Leaves/index.js
+++ router/Leaves/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { applyLeaveRouter } = require("./applyForLeave");
+const { getMyLeaveRouter } = require("./getMyLeaves");
 const { getLeaveRouter } = require("./getLeaves");
 
 const router = express.Router();
 
 router.use(applyLeaveRouter);
+router.use(getMyLeaveRouter);
 router.use(getLeaveRouter);
 
 module.exports = { LeaveRouter: router };
