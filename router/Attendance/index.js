6a3396025a3c06343ad9b94e0da08b9d37384049
hussain <hussainalsaffar20@gmail.com>
2025-01-26 13:41:07 +0300

    attendance index

--- router/Attendance/index.js
+++ router/Attendance/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const checkIn = require("./checkIn");
 const checkOut = require("./checkout");
+const getTodayAttendance = require("./getTodayAttendance");
 const router = express.Router();
 
 router.use(checkIn);
 router.use(checkOut);
+router.use(getTodayAttendance);
 
 module.exports = { AttendanceRouter: router };
